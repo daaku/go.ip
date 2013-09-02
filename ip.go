55b7876dc5fa31e431955daa9521bdfc354dfe55
Naitik Shah <n@daaku.org>
2013-09-01 23:51:13 -0700

    functional routing

--- ip.go
+++ ip.go
@@ -10,7 +10,7 @@ package main
 
 void activate_network_device() {
   struct ifreq ifr;
-  const char * name = "eth0";
+  char* name = "eth0";
   int fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
 
   strncpy(ifr.ifr_name, name, IFNAMSIZ);
@@ -20,25 +20,25 @@ void activate_network_device() {
   inet_pton(AF_INET, "192.168.100.4", &addr->sin_addr);
   ioctl(fd, SIOCSIFADDR, &ifr);
 
-  inet_pton(AF_INET, "255.255.0.0", &addr->sin_addr);
+  inet_pton(AF_INET, "255.255.255.0", &addr->sin_addr);
   ioctl(fd, SIOCSIFNETMASK, &ifr);
 
   ioctl(fd, SIOCGIFFLAGS, &ifr);
   strncpy(ifr.ifr_name, name, IFNAMSIZ);
   ifr.ifr_flags |= (IFF_UP | IFF_RUNNING);
-
   ioctl(fd, SIOCSIFFLAGS, &ifr);
 
   struct rtentry route;
+  route.rt_dev = name;
   addr = (struct sockaddr_in*) &route.rt_gateway;
   addr->sin_family = AF_INET;
   addr->sin_addr.s_addr = inet_addr("192.168.100.1");
   addr = (struct sockaddr_in*) &route.rt_dst;
   addr->sin_family = AF_INET;
-  addr->sin_addr.s_addr = inet_addr("0.0.0.0");
+  addr->sin_addr.s_addr = INADDR_ANY;
   addr = (struct sockaddr_in*) &route.rt_genmask;
   addr->sin_family = AF_INET;
-  addr->sin_addr.s_addr = inet_addr("0.0.0.0");
+  addr->sin_addr.s_addr = INADDR_ANY;
   route.rt_flags = RTF_UP | RTF_GATEWAY;
   route.rt_metric = 0;
   ioctl(fd, SIOCADDRT, &route);
